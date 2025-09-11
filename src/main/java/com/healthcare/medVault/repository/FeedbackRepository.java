5d9885f6a20e6c126e0f11b021d4207d5886c42c
Dhiraj Chaudhary <dhirajchaudhary199@gmail.com>
2025-09-11 11:11:30 +0530

    Added review system

--- src/main/java/com/healthcare/medVault/repository/FeedbackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.healthcare.medVault.repository;
-
-import com.healthcare.medVault.entity.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback,Long> {
-
-}
